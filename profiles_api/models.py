2b37c95861479412d1f42d3b4429de9eaeb293dd
lalit-vasoya <lalitvasoya.286@gmail.com>
2020-05-25 11:34:24 +0530

    Create admin user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -45,7 +45,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELS = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """ Return full name of user """
