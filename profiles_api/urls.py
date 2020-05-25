32ed03ce710e019d1ca678b12bb3f20304c24737
lalit-vasoya <lalitvasoya.286@gmail.com>
2020-05-25 18:51:55 +0530

    authentication permissions filter data and login view

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path,include
+
+from rest_framework.routers import DefaultRouter # use when viewset is used in our code
 
 from profiles_api import views
 
+router = DefaultRouter() #create object
+router.register('hello-viewset',views.HelloViewSet,basename='hello-view') #register view for router
+router.register('profile', views.UserProfileViewSet)
+
 urlpatterns = [
     path('hello-view/',views.HelloApiView.as_view(),name='hello_view'),
+    path('login/',views.UserLoginApiView.as_view(),name='login_view'),
+    path('',include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import  api_settings
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """ TEST Api View """
@@ -20,11 +27,9 @@ class HelloApiView(APIView):
 
     def post(self,request):
         """ Create hello message with our name """
-        print('Data :',request.data)
-        serializer = self.serializer_class(data=request.POST)
+        serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
-            print(serializer.validated_data)
             name = serializer.validated_data.get('name')
             message = f'Hello {name}'
             return Response({'message':message})
@@ -45,3 +50,65 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """ Delete an object """
         return Response({'message':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """ Test Api ViewSet """
+    serializer_class = serializers.HelloSerializer
+
+    def list(self,request):
+        an_viewset = [
+            'Use actions (list,create,retrieve,update,pratial_update)',
+            'Automatically maps to the Url router',
+            'provide many functionality with less code'
+        ]
+
+        return Response({'message':'Hello!','an_viewset':an_viewset})
+
+    def create(self,request):
+        """ Create hello message with name """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """ Handle getting object by IDS """
+        return Response({'message':'Retrieve'})
+
+    def update(self,request,pk=None):
+        """ Handling update an object """
+        return Response({'message':'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """ Handling partial update """
+        return Response({'message':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        """ Delete an object """
+        return Response({'message':'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Create and  Updating User Profile """
+    # for required configration
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    #for authentication and permission
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    #for filters
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email')
+
+class UserLoginApiView(ObtainAuthToken):
+    """ Handle create user authentication token """
+    print(api_settings.DEFAULT_RENDERER_CLASSES)
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
